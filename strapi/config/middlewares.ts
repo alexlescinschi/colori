f0b2f49d0d552d95befb3463f0a5642fa2310741
Alex <Alex.lescinschi@gmail.com>
2026-06-15 09:57:32 +0300

    feat: add Railway deployment config (PostgreSQL, CORS, pg driver)

--- strapi/config/middlewares.ts
+++ strapi/config/middlewares.ts
@@ -3,8 +3,30 @@ import type { Core } from '@strapi/strapi';
 const config: Core.Config.Middlewares = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'marketplace-images-production.s3-eu-west-1.amazonaws.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'marketplace-images-production.s3-eu-west-1.amazonaws.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['*'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
